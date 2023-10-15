39c0c8001fa8306bd027163010d5b6ba1debd0b6
Bhagya Raj Varadaraju <bhagyaraj.varadaraju@gmail.com>
2023-10-15 13:28:01 -0400

    Adding the trips controller and routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 
+import tripRoutes from './routes/trips.js'
+
 // create express app
 const app = express()
 
@@ -10,6 +12,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
 })
+
+app.use('/api/trips', tripRoutes)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
